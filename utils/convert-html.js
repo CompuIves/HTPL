60d8ddff6f78f9811736c0683397830f4e8cfe7b
Sven Mol <sven@taupeweb.nl>
2018-01-08 16:44:12 +0100

    Added product function

--- utils/convert-html.js
+++ utils/convert-html.js
@@ -35,6 +35,16 @@ function tagToValue(node) {
 
       return max;
     }
+    case 'product': {
+      const vars = node.childNodes.map(n => tagToValue(n)).filter(x => x);
+
+      const product = vars.reduce(
+        (product, value) => parseInt(value, 10) * product,
+        1
+      );
+
+      return product;
+    }
     case 'var': {
       const attr = node.attrs.find(s => s.name === 'name');
       if (!attr) {
@@ -81,6 +91,7 @@ export default function convertHTML(html: string) {
       }
       case 'min':
       case 'max':
+      case 'product':
       case 'sum': {
         const total = tagToValue(node);
 
